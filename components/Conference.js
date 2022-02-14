a13918e8c39136b8ad721ccc5ba44d87ca3eb95c
Himanshu Chahal <himanshuchahal25@gmail.com>
2022-02-14 21:17:06 +0530

    Conference updates

--- components/Conference.js
+++ components/Conference.js
@@ -1,6 +1,7 @@
 import { selectPeers, useHMSStore } from "@100mslive/hms-video-react";
 import React from "react";
 import Peer from "./Peer";
+import ChatContainer from '../Chat/ChatContainer'
 
 function Conference() {
     const peers = useHMSStore(selectPeers);
@@ -13,6 +14,8 @@ function Conference() {
                     <Peer key={peer.id} peer={ peer } />
                 ))}
             </div>
+
+            <ChatContainer/>
         </div>
     );
 }
